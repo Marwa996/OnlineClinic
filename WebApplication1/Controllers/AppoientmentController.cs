8b7b291bdc9a09774c8dc3446ef6e2e31c0e6308
omnia667 <oa840312@gmail.com>
2020-05-27 16:11:16 +0200

    show doctors , create appointments

---
+++ WebApplication1/Controllers/AppoientmentController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AppoientmentController : Controller
+    {
+        private HospitalDbContext db = new HospitalDbContext();
+
+
+        // GET: /Appointments/Create
+        [Authorize(Roles = "Patient")]
+        public ActionResult Create()
+        {
+            ViewBag.DoctorID = new SelectList(db.Doctors.Where(x => x.DisableNewAppointments == false), "ID", "Name");
+            ViewBag.TimeBlockHelper = new SelectList(String.Empty);
+            var model = new AppointmentModel
+            {
+                UserID = User.Identity.GetUserId()
+            };
+            return View(model);
+        }
+
+        // POST: /Appointments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Patient")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AppointmentID,DoctorID,Date,TimeBlockHelper,Time")] AppointmentModel appointment)
+        {
+            //Add userID
+            appointment.UserID = User.Identity.GetUserId();
+
+            //Verify Time
+            //if (appointment.TimeBlockHelper == "DONT")
+            //    ModelState.AddModelError("", "No Appointments Available for " + appointment.Date.ToShortDateString());
+            //else
+            //{
+
+            //    //Set Time
+            //    //appointment.Time = DateTime.Parse(appointment.TimeBlockHelper);
+
+            //    //CheckWorkingHours
+            //    DateTime start = appointment.Date.Add(appointment.Time.TimeOfDay);
+            //    DateTime end = (appointment.Date.Add(appointment.Time.TimeOfDay)).AddMinutes(double.Parse(db.Administrations.Find(1).Value));
+            //    if (!(BuisnessLogic.IsInWorkingHours(start, end)))
+            //        ModelState.AddModelError("", "Doctor Working Hours are from " + int.Parse(db.Administrations.Find(2).Value) + " to " + int.Parse(db.Administrations.Find(3).Value));
+
+            //    //Check Appointment Clash
+            //    string check = BuisnessLogic.ValidateNoAppoinmentClash(appointment);
+            //    if (check != "")
+            //        ModelState.AddModelError("", check);
+            //}
+
+            //Continue Normally
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+                return RedirectToAction("Details", new { Controller = "RegisteredUsers", Action = "Details" });
+            }
+
+            //Fill Neccessary ViewBags
+            ViewBag.DoctorID = new SelectList(db.Doctors.Where(x => x.DisableNewAppointments == false), "ID", "Name", appointment.DoctorID);
+            ViewBag.TimeBlockHelper = new SelectList(String.Empty);
+            return View(appointment);
+        }
+    }
+}
--- WebApplication1/Controllers/DoctorController.cs
+++ WebApplication1/Controllers/DoctorController.cs
@@ -1,19 +1,45 @@
-﻿using DoctorPatient.Infrastructure;
-using DoctorPatient.Models;
+﻿using WebApplication1.Infrastructure;
+using WebApplication1.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
-
+using WebApplication1.ViewModels;
+using WebApplication1.Models.Models;
 
 namespace WebApplication1.Controllers
 {
 	public class DoctorController : Controller
 	{
-        private HospitalDbContext db = new HospitalDbContext()
+        private HospitalDbContext db = new HospitalDbContext();
 
+
+
+        // GET: /Doctors/
+        public ActionResult Index(string docDept, string searchstring)
+        {
+            var deptList = new List<Enums.HospitalDepartment>();
+            deptList = Enum.GetValues(typeof(Enums.HospitalDepartment))
+                                    .Cast<Enums.HospitalDepartment>()
+                                    .ToList();
+            ViewBag.docDept = new SelectList(deptList);
+
+            var doctors = from d in db.Doctors
+                          select d;
+
+            if (!string.IsNullOrEmpty(searchstring))
+            {
+                doctors = doctors.Where(s => s.Name.Contains(searchstring));
+            }
+
+            if (!string.IsNullOrEmpty(docDept))
+            {
+                doctors = doctors.Where(x => x.Department.ToString() == docDept);
+            }
+            return View(doctors);
+        }
         // GET: /Doctors/UpcomingAppointments/5
         [Authorize(Roles = "Admin, Doctor")]
         public ActionResult UpcomingAppointments(string id, string SearchString)
